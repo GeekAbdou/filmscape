3e56b0104b77b5cf14ca59910c42dc78e1b28cff
Abd-Elrahman Elsayed <geek.abdoo@gmail.com>
2024-03-12 03:31:07 +0200

    initial commit

---
+++ src/componenets/App.jsx
@@ -0,0 +1,13 @@
+import { CssBaseline } from '@mui/material';
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <CssBaseline />
+      <h1>aaHiddi</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import { Route, Switch } from 'react-router-dom';
+
+import App from './componenets/App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
